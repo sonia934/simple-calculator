fc8a4f14caa91dafb15a228bd2cf906cae57ae4d
sonia934 <sonianiazi3009@gmail.com>
2024-09-03 17:09:56 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+
+function appendValue(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        
+        display.value = eval(display.value);
+    } catch (error) {
+        
+        display.value = 'Error';
+    }
+}
